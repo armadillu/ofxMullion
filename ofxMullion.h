859a48c56826ea867168a2ff373e2b026ede3674
Oriol Ferrer Mesià <oriol@uri.cat>
2014-04-24 19:25:26 -0400

    first

---
+++ ofxMullion.h
@@ -0,0 +1,52 @@
+//
+//  ofxMullion.h
+//  BaseApp
+//
+//  Created by Oriol Ferrer Mesià on 24/04/14.
+//
+//
+
+#ifndef __BaseApp__ofxMullion__
+#define __BaseApp__ofxMullion__
+
+
+#include "ofMain.h"
+
+class ofxMullion{
+
+	public:
+
+	void setup(int w, int h){
+		enabled = false;
+		m.setMode(OF_PRIMITIVE_LINES);
+		float stepX = 1.0f / (float) w;
+		float stepY = 1.0f / (float) h;
+		for(int i = 0; i <= w; i++){
+			m.addVertex( ofVec3f(i * stepX, 0.0f) );
+			m.addVertex( ofVec3f(i * stepX, 1.0f) );
+		}
+		for(int i = 0; i <= h; i++){
+			m.addVertex( ofVec3f(0.0f, stepY * i) );
+			m.addVertex( ofVec3f(1.0f, stepY * i) );
+		}
+	}
+
+	void toggle(){ enabled ^= true; }
+	void enable(){ enabled = true; }
+	void disable(){ enabled = false; }
+
+	void draw(){
+		if(enabled){
+			ofPushView();
+				ofSetupScreen();
+				ofScale(ofGetWidth(), ofGetHeight());
+				m.draw();
+			ofPopView();
+		}
+	}
+
+	ofVboMesh m;
+	bool enabled;
+};
+
+#endif /* defined(__BaseApp__ofxMullion__) */
